7f4411feea40c2eb860eb9d6b249e808cb43fa52
Narisa1 <Narisa1@users.noreply.github.com>
2025-07-23 13:41:30 +0700

    addworkshopfile

--- class03_workshopgame.py
+++ class03_workshopgame.py
@@ -1,59 +1,3 @@
-# for i in range(5):
-#     i + 1 
-#     print(i)
-
-#---------------------------------------------------
-
-# for i in range(1 , 10 ,2):
-#     print(i)
-
-
-#--------------------------------------------------
-
-# for i in range(5):
-#     print('halo')
-
-
-#--------------------------------------------------
-
-# sum = 0
-# n = 3
-
-# for  i in range(n):
-#     sum += i+1
-# print(sum)
-
-#--------------------------------------------------
-
-# for i in range( 1 , 10 ):
-#     if (i % 2) == 0:
-#         print(i, 'is even num')
-
-#--------------------------------------------------
-
-#WorkSHOP
-#ตารางสูตรคูณ
-
-# yournum = int(input('Input your num : '))
-
-# for i in range(1,13):
-#     ans = yournum * i
-#     print (yournum , 'x' ,i , '=' , ans )
-
-
-    # print(f"{yournum} * {i} = {ans}") การปริ้นโดยที่ไม่ให้ติดตัวแปรอีกแบบนึงโดนไม่ใช่ quote
-
-#------------------loopWHILE------------------------
-
-
-
-# i = 0
-# while i < 5:
-#     i = i + 1
-#     print('hello' , i)
-    
-#--------------------------------------------------
-
 monster = 100
 weapon1 = 20 
 weapon2 = 40 
@@ -95,22 +39,12 @@ while  start:
             print('Win')
         elif monster > 0:
             print('จำนวนตีหมดแต่มอนไม่แตก คนแตกแทน')
-            #     print('ตีแรงเกินมอนคืนชีพ!!!')
-            #     print('MONSTER' ,':', monster,'HP')
-            # else :
-            #     print('YOU WIN!!')
 
             
         break
-
 
     else:
         print('You Run Away!')
         break
             
     
-
-
-    
-
-
